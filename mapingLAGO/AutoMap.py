a89e27c8f6f48496d833589cf1a8072ae56cf474
Hernán Asorey <asoreyh@gmail.com>
2024-11-09 14:21:28 +0100

    Madrid Added. New coordinates. Fonts and legends improved. Markers improved. RC maps are not needed anymore. Large and small maps produced

--- mapingLAGO/AutoMap.py
+++ mapingLAGO/AutoMap.py
@@ -4,7 +4,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 ################ ignorar algun estado on,soon,uc desde el argumento ##
-ignore=[0,0,0]
+# uprunning, ongoing, planned, suspended, cancelled
+# suspended and cancelled ignored by default
+ignore=[0,0,0,1,1]
 for j in range(0,len(sys.argv)):
   if sys.argv[j]=='-uprunning':
      ignore[0]=1
@@ -12,21 +14,30 @@ for j in range(0,len(sys.argv)):
     ignore[1]=1
   if sys.argv[j]=='-planned':
     ignore[2]=1
-ig=ignore[0]+ignore[1]+ignore[2]
+  if sys.argv[j]=='-suspended':
+    ignore[3]=0
+  if sys.argv[j]=='-cancelled':
+    ignore[4]=0
 
-if ig==3:
+ig=ignore[0]+ignore[1]+ignore[2]+ignore[3]+ignore[4]
+
+if ig==5:
   sys.exit("ERROR--IGNORANDO TODO!!!")
   
 ##################### define el marker y color segun el estado #######
 
 def estatus(x):
-  status=(0,0)
-  if x=='uprunning':
+  status=(0, 0)
+  if x == 'uprunning':
     status=('^','#0033CC')
-  elif x=='ongoing':
+  elif x == 'ongoing':
     status=('s','#FF8C00')
-  elif x=='planned':
+  elif x == 'planned':
     status=('o','#CC0000')
+  elif x == 'suspended':
+    status=('-','#CCCC00')
+  elif x == 'cancelled':
+    status=('x','#CCCCCC')
   else:
     print (x)
     sys.exit("ERROR--ESTAUS DE SITIO NO VALIDO!!!")
@@ -40,7 +51,20 @@ files=open('misdatos.dat')
 
 
 #### genera mapa basico  #################################################
-map = Basemap(projection='cyl',llcrnrlat=-84,urcrnrlat=33.56,llcrnrlon=-118.25,urcrnrlon=-33.56,resolution='l')
+# map = Basemap(projection='cyl',llcrnrlat=-84,urcrnrlat=33.56,llcrnrlon=-118.25,urcrnrlon=-33.56,resolution='l')
+lowerleftlat=-75
+lowerleftlon=-110
+upperrightlat=+55
+upperrightlon=+20
+
+map = Basemap(
+  projection='cyl',
+  llcrnrlat=lowerleftlat,
+  llcrnrlon=lowerleftlon,
+  urcrnrlat=upperrightlat,
+  urcrnrlon=upperrightlon,
+  resolution='l'
+)
 plt.figure(figsize=(13,18))
 
 map.drawcoastlines(linewidth=.5)
@@ -81,47 +105,76 @@ for line in files:
   if ig>0:
     if ignore[0]>0 and row[4]=='uprunning': #ignora on
       continue
-    if ignore[1]>0 and row[4]=='ongoing':#ignora soon
+    if ignore[1]>0 and row[4]=='ongoing':#ignora ongoing
       continue
     if ignore[2]>0 and row[4]=='planned': #ingnora uc
-      #print row[0],ignore
       continue
-  if row[0][0]=='#':
+    if ignore[3]>0 and row[4]=='suspended':#ignora suspended
+      continue
+    if ignore[4]>0 and row[4]=='cancelled':#ignora cancelled
+      continue
+  if row[0][0] == '#':
     continue
-  if row[1]=='':
-    continue
-  cont+=1
-  lat=float(row[2])
-  lon=float(row[3])
+  cont += 1
+  lat = float(row[2])
+  lon = float(row[3])
   tatus=estatus(row[4]) # determina color y marker segun estatus
-  x,y = map(lon,lat)
-  map.scatter(x,y,marker=tatus[0],facecolor=tatus[1][0:7],s=150,zorder=10)#grafica los puntos
-  #print row[0], x,xaux, y,yaux
-  cornu=crosletras(y,yaux,x,xaux,len(row[1]),cont)
-  #print crosletras(y,yaux,x,xaux,len(row[1]),cont)
-  m=((-1)**(cont))
-  plt.text(-117,13-(2.6*cont),str(cont)+' = '+row[0]+' ('+str(int(float(row[1])))+' m)',color='w',fontsize=17,zorder=10, fontweight='bold')#grafica la leyenda que numero-lugar en blanco a la derecha
-  if cont==5 or cont==14:
-    plt.text(x+(m*3.5)-((0.5+(cont/10))*m)+m*(cont/10)-1.2,y-2,str(cont),color='black',fontsize=12,zorder=15,fontweight='bold')# grafica los nuero 5 y 14 mas cerca de los marker esto tal vez de ba quitarse ###############
-    continue
+  x, y = map(lon, lat)
 
-  plt.text(x+(m*3)-((0.5+(cont/10))*m)+m*(cont/10)-1.2,y-0.5,str(cont),color='black',fontsize=12,zorder=15,fontweight='bold')# grafica numeros impares a la izquierda del marker y pares a la derecha
+  map.scatter(
+    x,
+    y,
+    marker=tatus[0],
+    facecolor=tatus[1][0:7],
+    s=150,
+    zorder=10
+  )
+  # cornu=crosletras(y,yaux,x,xaux,len(row[1]),cont)
+# markers
+# grafica numeros impares a la izquierda del marker y pares a la derecha
+  m=((-1)**(cont))
+  plt.text(
+    x + (m * 3) - ((0.5 + (cont / 10)) * m) + m * (cont / 10) - 1.3,
+    y - 0.5,
+    ('0' if cont < 10 else '') + str(cont),
+    color='#000000',
+    fontsize=10,
+    zorder=15,
+    fontfamily='monospace',
+    fontweight='normal'
+  )
+
+# Leyenda
+  # plt.text(-117,13-(2.6*cont),str(cont)+' = '+row[0]+' ('+str(int(float(row[1])))+' m)',color='w',fontsize=17,zorder=10, fontweight='bold')#grafica la leyenda que numero-lugar en blanco a la derecha
+  plt.text(
+    -30.0 , 
+    +25.0 - (2.9 * cont), 
+    ('0' if cont < 10 else '') + str(cont) + '. ' + row[0] + ' ('+str(int(float(row[1])))+' m)', 
+    zorder=10, 
+    color='#ffffff', 
+    fontsize=15, 
+    fontfamily='monospace',
+    fontweight='bold'
+  )
+
 
 ##################### genera etiquetas para la leyenda superior sobre los puntos##############################
 xi=23
 yi=23
+fs=150
 xi,yi=map(lon,lat)
 if ignore[0]==0:
-  plt.scatter(xi,yi,marker='^',s=200,color='#0033CC',label='Stable')
+  plt.scatter(xi, yi, marker='^', s=fs, color='#0033CC', label='LAGO Site')
 if ignore[2]==0:
-  plt.scatter(xi,yi,marker='s',s=200,color='#FF8C00',label='Deploying')
+  plt.scatter(xi, yi, marker='s', s=fs, color='#FF8C00', label='Deploying')
 if ignore[1]==0:
-  plt.scatter(xi,yi,marker='o',s=200,color='#CC0000',label='Planned')
-lg = plt.legend(loc='upper right', fontsize=26,scatterpoints=1)
-lg.get_frame().set_alpha(.8) # A little transparency
-
+  plt.scatter(xi, yi, marker='o', s=fs, color='#CC0000', label='Planned')
+if ignore[3]==0:
+  plt.scatter(xi, yi, marker='x', s=fs, color='#CCCC00', label='Suspended')
+if ignore[4]==0:
+  plt.scatter(xi, yi, marker='-', s=fs, color='#CCCCCC', label='Cancelled')
+lg = plt.legend(loc='upper left', fontsize=20, scatterpoints=1)
+lg.get_frame().set_alpha(.6) # A little transparency
 
 os.system("rm misdatos.dat")
 plt.savefig('lagoplano.png',dpi=300,bbox_inches='tight', pad_inches = 0)
-
-#plt.show()
